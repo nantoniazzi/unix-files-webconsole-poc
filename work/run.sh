09f5e88abef10a4a2a6e0900a42a4abbb05a0068
Nicolas Antoniazzi <nicolas@codingame.com>
2017-04-11 00:02:51 -0700

    update

--- work/run.sh
+++ work/run.sh
@@ -3,4 +3,18 @@ echo "CG> redirect-streams null"
 /etc/init.d/apache2 start
 echo "CG> redirect-streams -r"
 echo "CG> open -p 80 /webconsole.php"
-./validate.sh
+
+#function checkFileExists {
+#  FILE=$1
+#  FINISHED=0
+#  while [ $FINISHED -eq 0 ]; do
+#    if [ -e "$FILE" ]
+#    then
+#      echo "CG> success true"
+#      FINISHED=1
+#    fi
+#    sleep 0.1
+#  done 
+#}
+
+#checkFileExists "/var/www/html/foo/bar/valid_file"
