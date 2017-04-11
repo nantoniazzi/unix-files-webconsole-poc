c9aaa42dc72c065aada265a1eff59c933baca7c1
Nicolas Antoniazzi <nicolas@codingame.com>
2017-04-11 01:22:08 -0700

    up

--- work/webconsole.php
+++ work/webconsole.php
@@ -779,7 +779,7 @@ class WebConsoleRPCServer extends BaseJsonRpcServer {
         $result = $this->initialize($token, $environment);
         if ($result) return $result;
 
-        $output = ($command && !is_empty_string($command)) ? execute_command('sudo ' . $command) : '';
+        $output = ($command && !is_empty_string($command)) ? execute_command('sudo ' .  $command) : '';
         if ($output && substr($output, -1) == "\n") $output = substr($output, 0, -1);
 
         return array('output' => $output);
